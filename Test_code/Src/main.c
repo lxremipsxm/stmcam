8d6bf11c0a7e3037398cd3471c63d201e24fac42
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-05-25 08:42:49 -0400

    Finalized file structure and successfully flashed dummy code

--- Test_code/Src/main.c
+++ Test_code/Src/main.c
@@ -1,8 +1,27 @@
+/*Siddharth Vasudevan
+Test_code/src/main.c
+*/
+
 #include "stm32f4xx.h"
 #include <stdint.h> 
- 
- int main(void) {
 
-    volatile int x=0;
-    while(1) x++;
- }
+void delay(volatile uint32_t count) {
+    while (count--) {
+        __asm__("nop"); //no operation on current clock cycle
+    }
+}
+
+int main(void) {
+    //Enable GPIOA
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+    //Config PA5 as output
+    GPIOA->MODER &= ~(3UL << (5 * 2));   
+    GPIOA->MODER |=  (1UL << (5 * 2)); 
+
+    while (1) {
+        // Toggle PA5
+        GPIOA->ODR ^= (1 << 5);
+        delay(1000000);
+    }
+}
