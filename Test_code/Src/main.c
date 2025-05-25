a21eb2e663d1d59b5ec44984a8b58038fb0ccf53
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-05-25 03:25:14 -0400

    Rearranged project directory, added test file that successfully flashes to the board

--- Test_code/Src/main.c
+++ Test_code/Src/main.c
@@ -1,8 +1,8 @@
-#include "stm32f401xe.h"
-#include "CMSIS/core_cm4.h"
-#include "system_stm32f4xx.h"
+#include "stm32f4xx.h"
+#include <stdint.h> 
+ 
+ int main(void) {
 
-int main(void){
-    int x=4,y=5;
-    int z = x+y;
-}
+    volatile int x=0;
+    while(1) x++;
+ }
